1605f9d34b7ec6e01f799d0c7d879bbe5a9df922
Jone Doe <test@example.com>
2018-08-13 20:20:21 +0900

    asdf

--- layer/ssd.py
+++ layer/ssd.py
@@ -1,8 +1,26 @@
 import torch
+import torch.nn as nn
 import torchvision
 
 class SSD(torch.nn.Module):
     def __init__(self):
-        self.vgg = torchvision.models.vgg16(pretrained=True)
+        super(SSD,self).__init__()
+        self.vgg = torchvision.models.vgg16(pretrained=True).features
 
-        self.
+        self.extra = nn.Sequential(
+            nn.Conv2d(),
+        )
+
+    def forward(self, x):
+        #input -> vgg -> extra -> class
+        #                      ->
+        
+        for i in range():
+            x = self.vgg[i](x)
+            if i in (21, 30):
+                asdf.append(x)
+        
+        for i in range():
+            x = self.extra[i](x)
+            if i in ():
+                asdf.append(x)
