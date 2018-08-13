cfc72b9a0bcbdc744815f432409caa11adb0acd7
consilium538 <dhej1dark@gmail.com>
2018-08-13 13:40:36 +0900

    anno_script.py debug

--- anno_script.py
+++ anno_script.py
@@ -21,9 +21,9 @@ annopath = basepath / 'Annotations'
 dumppath = Path(f'./anno_{cfg["year"]}_{cfg["set"]}.bin')
 
 def classlabel(classname):
-    return next(filter(lambda x: x[1] == classname, enumerate(vocclasses)))
+    return next(filter(lambda x: x[1] == classname, enumerate(vocclasses)))[0]
 
-def setname(setpath):
+def setname(setfile):
     with open(setfile, 'r') as fp:
         annolist = fp.readlines()
     return map(lambda x: x.strip(), annolist)
@@ -34,18 +34,18 @@ def annoparse(annopath):
     bboxlist = list()
     for i in lxmltree.xpath('//object'):
         bboxlist.append((
-                    classlabel(i.xpath('name/text()')[0])
-                    i.xpath('bndbox/xmin/text()')[0]
-                    i.xpath('bndbox/ymin/text()')[0]
-                    i.xpath('bndbox/xmax/text()')[0]
+                    classlabel(i.xpath('name/text()')[0]),
+                    i.xpath('bndbox/xmin/text()')[0],
+                    i.xpath('bndbox/ymin/text()')[0],
+                    i.xpath('bndbox/xmax/text()')[0],
                     i.xpath('bndbox/ymax/text()')[0]
                 ))
-    return (fielname, bboxlist)
+    return (filename, bboxlist)
 
 def anno_script():
     anno_list = list()
     for i in setname(setpath):
-        anno_list.append(annoparse(annopath / f'{i}.xml'))
+        anno_list.append(annoparse(str(annopath / f'{i}.xml')))
     with open(dumppath,'wb') as fp:
         pickle.dump(anno_list,fp)
     return
