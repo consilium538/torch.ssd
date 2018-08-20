edb26c5ab21b210baae4632d78c855f7236350fc
consilium538 <dhej1dark@gmail.com>
2018-08-20 13:13:15 +0900

    repair ssd.py

--- layer/modules/ssd.py
+++ layer/modules/ssd.py
@@ -1,16 +1,19 @@
 import torch
 import torch.nn as nn
 import torchvision
+import numpy as np
+from itertools import product
+from math import sqrt
 
 # pytorch conv2 format : (N, C, H, W), vgg pretrained color : (R, G, B)
 # normalized with mean = [0.485, 0.456, 0,406], std = [0.299, 0.224, 0.225]
 
-f_map = ((38, 512, 4, (2)),
+f_map = ((38, 512, 4, (2,)),
         (19, 1024, 6, (2, 3)),
         (10, 512, 6, (2, 3)),
         (5, 256, 6, (2, 3)),
-        (3, 256, 4, (2)),
-        (1, 128, 4, (2)))
+        (3, 256, 4, (2,)),
+        (1, 256, 4, (2,)))
 num_classes = 21
 
 class SSD(nn.Module):
@@ -21,9 +24,9 @@ class SSD(nn.Module):
         """
         super(SSD,self).__init__()
         self.vgg = torchvision.models.vgg16(pretrained=True).features[:-1]
-        for i in vgg.parameters(): #freeze pretrained network
+        for i in self.vgg.parameters(): #freeze pretrained network
             i.requires_grad = False
-        for i in vgg:
+        for i in self.vgg:
             if isinstance(i, nn.MaxPool2d): # make dimension correct
                 i.ceil_mode = True
 
@@ -54,27 +57,27 @@ class SSD(nn.Module):
         loc_list = list()
         conf_list = list()
 
-        for i in range():
+        for i in range(29):
             x = self.vgg[i](x)
-            if i in (21):
+            if i in (21,):
                 feature_list.append(x)
 
-        for i in range():
+        for i in range(10):
             x = self.extra[i](x)
             if i % 2 == 1:
                 feature_list.append(x)
 
         for (i,j,k) in zip(feature_list, self.mbox_loc, self.mbox_conf):
-            loc_list.append(j(i).permute(0,2,3,1))
-            conf_list.append(k(i).permute(0,2,3,1))
+            loc_list.append(j(i).permute(0,2,3,1).contiguous())
+            conf_list.append(k(i).permute(0,2,3,1).contiguous())
 
-        loc = torch.cat([t.view(t.size(0),-1) for in loc_list],1)
-        conf = torch.cat([t.view(t.size(0),-1) for in conf_list],1)
+        loc = torch.cat([t.view(t.shape[0],-1) for t in loc_list],1)
+        conf = torch.cat([t.view(t.shape[0],-1) for t in conf_list],1)
 
         output = (
                 loc.view(loc.size(0),-1,4),
-                conf.view(conv.size(0),-1,num_classes),
-                torch.Tensor(self.default_box).view(-1.4)
+                conf.view(conf.size(0),-1,num_classes),
+                torch.Tensor(self.default_box).view(-1,4)
         )
 
         return output
@@ -87,12 +90,12 @@ class SSD(nn.Module):
 
         #num_default = reduce(lambda x,y:x+y, (x[0]*x[0]*x[2] for x in f_map))
         box_size = np.linspace(0.2, 0.9, 6) # 가장 큰 defualt box의 s_k`의 경우 scale 계산 어찌?
-        box_prime_size = np.linsapce(0.34, 1.04, 6)
+        box_prime_size = np.linspace(0.34, 1.04, 6)
         box_prime_size = np.sqrt(box_size,box_prime_size)
         default_box = list()
         for k, f in enumerate(f_map):
-            for i, j in product(range(f), repeate=2):
-                cx, cy = i+0.5/f , j+0.5/f
+            for i, j in product(range(f[0]), repeat=2):
+                cx, cy = (i+0.5)/f[0] , (j+0.5)/f[0]
 
                 s_k = box_size[k]
                 default_box.append((cx,cy,s_k,s_k))
@@ -101,5 +104,6 @@ class SSD(nn.Module):
                 default_box.append((cx,cy,s_k_p,s_k_p))
                 for r in f[3]:
                     default_box.append((cx,cy,s_k*sqrt(r),s_k/sqrt(r)))
+                    default_box.append((cx,cy,s_k/sqrt(r),s_k*sqrt(r)))
 
         return default_box
