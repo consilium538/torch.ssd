f10f156a1c1a233eed3c0dcc140072d9adf4cbce
D.H. Kim <dhej1dark@gmail.com>
2018-08-28 20:01:29 +0900

    fxxk

--- train.py
+++ train.py
@@ -1,7 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import torch;import torch.nn as nn;import torch.nn.functional as F
 torch.set_default_tensor_type(torch.cuda.FloatTensor)
 from layer.modules.ssd import SSD
 from layer.modules.loss import SSDLoss
 from layer.function.iou import match
 
-
+# load weight
+net = SSD()
+if os.path.isfile(arg['weightpath']):
+    net.load_state_dict(torch.load(arg['wegihtpath']))
+    print('Pervious trainded weight loaded')
+else:
+    net.vgg
+# dataloader
+# for i in range(epoch)
+#   for j in dataloader
+#       a = ssd(j)
+#       loss = criterion(a,j)
+#       optim.zero_grad()
+#       loss.backword
+#       optim.step()
+#   save weight
